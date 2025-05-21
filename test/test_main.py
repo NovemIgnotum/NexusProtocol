8b05fa79daec9469b2fd92af98fd76d037bef644
Baptiste <102555318+NovemIgnotum@users.noreply.github.com>
2025-05-21 17:05:12 +0200

    volounter failure in test

--- test/test_main.py
+++ test/test_main.py
@@ -2,6 +2,6 @@ def test_true_is_true():
     assert True == True
 
 def test_hello_world(capsys):
-    print("Hello, world!")
+    print("Hello, le monde")
     captured = capsys.readouterr()
     assert captured.out == "Hello, world!\n"
