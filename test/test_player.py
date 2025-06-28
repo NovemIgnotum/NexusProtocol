51fdac3391bba082fa3a32009891461360a7a3f6
Nicolas Daunac <97985114+Farfadeli@users.noreply.github.com>
2025-06-28 20:12:45 +0200

    add player test

--- test/test_player.py
+++ test/test_player.py
@@ -5,6 +5,12 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 from game import player
 from game import utils
 
+def test_negative_heal():
+    p1 = player.Character("Nicolas", "Magicien")
+    p1.heal(-54)
+
+    assert p1.health == 21
+
 def test_damage_check() :
     p1 = player.Character("Nicolas", "Magicien") # Le magicien à une attaque de 10 et 75 point de vie
     p2 = player.Character("Baptiste" , "Magicien")
